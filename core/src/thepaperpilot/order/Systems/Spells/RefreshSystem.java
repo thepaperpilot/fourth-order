1851cdfa4d0456df85cc7b12db90b323e64b3256
thepaperpilot <thepaperpilot@gmail.com>
2016-04-03 23:37:28 -0500

    fixed bug with refresh spell

--- core/src/thepaperpilot/order/Systems/Spells/RefreshSystem.java
+++ core/src/thepaperpilot/order/Systems/Spells/RefreshSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import thepaperpilot.order.Components.DestroyComponent;
+import thepaperpilot.order.Components.FighterComponent;
 import thepaperpilot.order.Components.PuzzleComponent;
 import thepaperpilot.order.Components.Spells.RefreshComponent;
 import thepaperpilot.order.Util.Mappers;
@@ -16,11 +17,12 @@ public class RefreshSystem extends SpellSystem {
 
     protected void castSpell(Entity entity) {
         RefreshComponent rc = Mappers.refresh.get(entity);
+        FighterComponent fc = Mappers.fighter.get(entity);
         PuzzleComponent pc = Mappers.puzzle.get(entity);
 
         for (int i = 0; i < rc.number; i++) {
             Entity random = pc.puzzle.randomRune();
-            random.add(new DestroyComponent(pc.puzzle.player));
+            random.add(new DestroyComponent(fc));
         }
     }
 }
